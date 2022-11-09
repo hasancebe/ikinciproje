7494c85d5dad691d5dd7360a1f543603ab77acc2
hasan cebe <hasancebe237@gmail.com>
2022-11-10 00:01:26 +0300

    değişiklik

---
+++ src/ikinci.java
@@ -0,0 +1,5 @@
+public class ikinci {
+    public static void main(String[] args) {
+        int a=0;
+    }
+}
---
+++ src/ilk.java
@@ -0,0 +1,5 @@
+public class ilk {
+    public static void main(String[] args) {
+
+    }
+}
