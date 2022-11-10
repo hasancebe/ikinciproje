11e1fb7ec676d0b7f9470a410cf19d13c3608c19
hasan cebe <hasancebe237@gmail.com>
2022-11-10 20:07:12 +0300

    bes

--- src/dordclass.java
+++ src/dordclass.java
@@ -1,5 +1,12 @@
 public class dordclass {
-    public static void main(String[] args) {
 
+
+
+   static String name="hasan";
+    public static void main(String[] args) {
+//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
+dordclass obj=new dordclass();
+
+        System.out.println(dordclass.name);
     }
 }
